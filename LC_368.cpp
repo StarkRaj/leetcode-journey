067f1b4a74aa6a61b2aaaa41c060431c07ce0d56
Stark <nitishraj2706@gmail.com>
2026-02-25 23:11:07 +0530

    Largest Divisible subset [DP]

---
+++ LC_368.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int vSize = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> dp(vSize, 1);
+        int maxSize = 1, parent = -1;
+        for(int i=1; i<vSize; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    maxSize = max(maxSize, dp[i]);
+                }
+            }
+        }
+        vector<int> ans;
+        for(int i=vSize-1; i>=0; i--){
+            if(dp[i] == maxSize){
+                if(parent == -1 || parent % nums[i] == 0){
+                    ans.push_back(nums[i]);
+                    parent = nums[i];
+                    maxSize--;
+                }
+            }
+        }
+        return ans;
+    }
+};
